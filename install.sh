6d67db328180e1a7a9cf108fa4b1801c03219181
Terry Chia <terrycwk1994@gmail.com>
2020-06-16 11:26:40 +0800

    Fix install.sh.

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 mkdir -p $HOME/tools
-ln -sf $DIR/bin $HOME/tools/bin
-ln -sf $DIR/android $HOME/tools/android
+ln -sf $DIR/bin $HOME/tools
+ln -sf $DIR/android $HOME/tools
